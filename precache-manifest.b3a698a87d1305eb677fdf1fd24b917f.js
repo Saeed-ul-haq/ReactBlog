fe46a8a62b0b2e3775a62568fbb408e252fe33f9
Saeed ul Haq <33142166+Saeed-ul-haq@users.noreply.github.com>
2020-06-08 12:22:48 +0500

    Updates

---
+++ precache-manifest.b3a698a87d1305eb677fdf1fd24b917f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb4a142e7ce8b0d35cc4f6aba681004",
+    "url": "/ReactBlog/index.html"
+  },
+  {
+    "revision": "70c09e8cb77fe19b6ab5",
+    "url": "/ReactBlog/static/css/main.c16a26f9.chunk.css"
+  },
+  {
+    "revision": "bdc6afd6452e59b4a3ee",
+    "url": "/ReactBlog/static/js/2.005a4c83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactBlog/static/js/2.005a4c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c09e8cb77fe19b6ab5",
+    "url": "/ReactBlog/static/js/main.893a5653.chunk.js"
+  },
+  {
+    "revision": "49c628ffd92cf5ed930b",
+    "url": "/ReactBlog/static/js/runtime-main.d50efa7e.js"
+  }
+]);
